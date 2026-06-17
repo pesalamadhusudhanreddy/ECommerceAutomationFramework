368e4f1aa4e3df4f50b0e5d0508ec02405763836
pesala madhusudhan reddy <pesalamadhusudhanreddy@gmail.com>
2026-06-17 09:18:48 +0530

    Fix Extent report generation path

---
+++ ECommerce.Utilities/Reports/ExtentManager.cs
@@ -0,0 +1,35 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace ECommerce.Utilities.Reports
+{
+    public class ExtentManager
+    {
+        public static ExtentReports Extent;
+
+        public static ExtentReports GetExtent()
+        {
+            if (Extent == null)
+            {
+                string path = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "Reports",
+                    "index.html"
+                );
+
+                Directory.CreateDirectory(
+                    Path.GetDirectoryName(path)
+                );
+
+                var reporter =
+                    new ExtentSparkReporter(path);
+
+                Extent = new ExtentReports();
+
+                Extent.AttachReporter(reporter);
+            }
+
+            return Extent;
+        }
+    }
+}
